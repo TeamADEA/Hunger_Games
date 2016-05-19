5c2c3f6d468d854bf58e7d1e6a325bc5e52d75a8
mgr44 <m.grant.ritchie@gmail.com>
2016-05-18 19:50:13 -0700

    last minute

--- SimManager.py
+++ SimManager.py
@@ -2,16 +2,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as col
 import Hunger_Grid as hg
-import Kat
+from Kat import Kat
+from hg_settings import *
 
+GRID_DIMENSION = 34
+NUM_KATS = 20
+
+#MOVE = {[0,0]:"NOTHING", [-1,0]:"UP", []:"RIGHT", []:"DOWN", []:"LEFT"}
+MOVE = [[-1,0],[0,1],[1,0],[0-1]]
 class simManager():
     grid = None
+    kats = []
     
-    
-    def __init__(self):
-        self.grid = hg.createHungerGrid()
+    def __init__(self, seedKat):
+        self.grid = hg.createHungerGrid(GRID_DIMENSION,GRID_DIMENSION)
+        self.kats.append(seedKat)
+        for i in range(1, NUM_KATS):
+            if(i <= 4):
+               self. kats.append(seedKat.clone())
+            """
+            else:
+                kats.append(mutateKat(seedKat.cloe()))
+            """
+        for k in self.kats:
+            self.setKatPosition(k)
+    def update(self):
+        for k in kats:
+            direction = MOVE[k.makeDecision(self.grid)]
+            nextX = k.xLoc + direction[1]
+            nextY = k.yLoc + direction[0]
+            grid[kat.y,kat.x] = GRASS
+            k.take_step(nextY, nextX)
+            
+            if(self.grid[k.yLoc,k.xLoc] == LAVA):
+                k.dead = True
+            elif(self.grid[k.yLoc,k.xLoc] == BERRY):
+                k.eat_berries
+                
+    def setKatPosition(self, kat):
+        randX = np.random.randint(2, GRID_DIMENSION - 2)
+        randY = np.random.randint(2, GRID_DIMENSION - 2)
+        
+        if(self.grid[randY,randX] == GRASS):
+            kat.xLoc = randX
+            kat.yLoc = randY
+            self.grid[randY, randX] = 3
+            return
+        else:
+            self.setKatPosition(kat)
 
-    def visualize(self,hungerGrid, katArray):
+    def visualize(self):
         ################################################################################
         plt.ion()
         fig = plt.figure(figsize=(6,6))
@@ -23,5 +63,9 @@ class simManager():
         KATS_KOLOR    = '#EEFF11'
         WALL_COLOR    = '#696969'
         hunger_color = col.ListedColormap([GRASS_COLOR, LAVA_COLOR, BERRY_COLOR, KATS_KOLOR, WALL_COLOR])
-        img = ax.imshow(hungerGrid, cmap= hunger_color, interpolation='none')
+        img = ax.imshow(self.grid, cmap= hunger_color, interpolation='none')
         plt.pause(.1) 
+        
+kat = Kat(0,0)
+test = simManager(kat)
+test.visualize()
