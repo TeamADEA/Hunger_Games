09794f73035d15d022ab6b4974a415fe26984a6d
klinsmUW <klinuw@uw.edu>
2016-05-25 14:22:46 -0700

    added some Kat and Hg_Settings Test

--- UnitTest.py
+++ UnitTest.py
@@ -10,15 +10,20 @@ from Kat import Kat
 from Visualize import Visualizer
 from SimManager import sim_manager
 from Hunger_Grid import hunger_grid
-from hg_settings import *
+import hg_settings as hgs
 
 class TestCaseSuperClass(unittest.TestCase):
     def setUp(self):
         pass
         
 class KatTestCase(TestCaseSuperClass):
-    def runTest(self):
-        pass
+    def test_kat_init(self):
+        self.kat = Kat()
+        self.assertEqual(self.kat.xLoc, 0, "Kat initial x location is incorrect")
+        self.assertEqual(self.kat.yLoc, 0, "Kat initial y location is incorrect")
+        self.assertEqual(self.kat.steps_taken, 0, "Steps taken should be initialized to 0")
+        self.assertEqual(self.kat.berries_eaten, 0, "Berries eaten should be initialize to 0")
+        self.assertFalse(self.kat.dead, "Kat should not be dead")
 
 class VisualizeTestCase(TestCaseSuperClass):
     def runTest(self):
@@ -33,8 +38,17 @@ class HungerGridTestCase(TestCaseSuperClass):
         pass
 
 class HGSettingsTestCase(TestCaseSuperClass):
-    def runTest(self):
-        pass
+    def test_correct_global_var(self):
+        self.assertEqual(hgs.UP, 0, "Up code should be 0")
+        self.assertEqual(hgs.RIGHT, 1, "Right code should be 1")
+        self.assertEqual(hgs.DOWN, 2, "Down code should be 2")
+        self.assertEqual(hgs.LEFT, 3, "Left code should be 3")
+        self.assertEqual(hgs.DO_NOTHING, 4, "Do_nothing code should be 4")
+        self.assertEqual(hgs.GRASS, 0, "Grass code should be 0")
+        self.assertEqual(hgs.LAVA, 1, "Lava code should be 1")
+        self.assertEqual(hgs.BERRY, 2, "Berry code should be 2")
+        self.assertEqual(hgs.KAT, 3, "Kat code should be 3")
+        self.assertEqual(hgs.WALL, 4, "Wall code should be 4")
         
 class MutateTestCase(TestCaseSuperClass):
     def runTest(self):
